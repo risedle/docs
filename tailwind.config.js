4b56d261b978f9c3aa17b36aa24cd5b49e1f1284
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2021-10-12 22:40:57 +0700

    ✦ Setup Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+    purge: [
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
+    ],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            fontFamily: {
+                inter: ["'Inter'", "sans-serif"],
+            },
+            colors: {
+                blue: {
+                    DEFAULT: "#2D81FF",
+                },
+            },
+            opacity: {
+                8: ".08",
+                15: ".15",
+                95: ".95",
+            },
+        },
+    },
+    variants: {
+        extend: {
+            opacity: ["hover"],
+            scale: ["active"],
+        },
+    },
+    plugins: [],
+};
